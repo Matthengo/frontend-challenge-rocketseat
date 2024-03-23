06abdfd0ed3bc4e0c2faabf233d6c8a60bceac6d
Matthengo <matheus.hengo@gmail.com>
2024-03-23 20:50:02 -0300

    FEAT: adjusts main page responsive

--- capputeeno/src/components/Main.styled.tsx
+++ capputeeno/src/components/Main.styled.tsx
@@ -1,10 +1,19 @@
+import { mediaQueries } from "@/utils/mediaQueries";
 import styled from "styled-components";
 
 export const Main = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 34px 160px;
+  padding: 34px 10px;
   min-height: 100vh;
   background-color: var(--page-bg-color);
+
+  @media ${mediaQueries.sm} {
+   padding: 34px 40px; 
+  }
+
+  @media ${mediaQueries.xl} {
+    padding: 34px 160px;
+  }
 `
